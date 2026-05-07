098a8c5b32c6b614528905a524851570843c8c3e
Matheus Chiqueto <matheuschiqueto@hotmail.com>
2026-05-07 10:38:00 -0300

    feat: implementa Fila FIFO com nos encadeados

---
+++ fila.py
@@ -0,0 +1,31 @@
+from modelos import NodoFila
+
+
+class Fila:
+    def __init__(self):
+        self._inicio = None
+        self._fim = None
+        self._tamanho = 0
+
+    def enqueue(self, musica):
+        nodo = NodoFila(musica)
+        if self._fim is None:
+            self._inicio = nodo
+            self._fim = nodo
+        else:
+            self._fim.proximo = nodo
+            self._fim = nodo
+        self._tamanho += 1
+
+    def dequeue(self):
+        if self._inicio is None:
+            return None
+        musica = self._inicio.musica
+        self._inicio = self._inicio.proximo
+        if self._inicio is None:
+            self._fim = None
+        self._tamanho -= 1
+        return musica
+
+    def tamanho(self):
+        return self._tamanho

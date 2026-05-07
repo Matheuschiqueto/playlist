c216c84595cd7418d0167d8c4446b3d6df840b96
Matheus Chiqueto <matheuschiqueto@hotmail.com>
2026-05-07 09:53:00 -0300

    feat: adiciona classe Musica e nos encadeados

---
+++ modelos.py
@@ -0,0 +1,22 @@
+class Musica:
+    def __init__(self, id, titulo, artista, genero, bpm):
+        self.id = id
+        self.titulo = titulo
+        self.artista = artista
+        self.genero = genero
+        self.bpm = bpm
+
+    def __str__(self):
+        return f"[{self.id}] {self.titulo} — {self.artista} | {self.genero} | {self.bpm} BPM"
+
+
+class NodoLista:
+    def __init__(self, musica):
+        self.musica = musica
+        self.proximo = None
+
+
+class NodoFila:
+    def __init__(self, musica):
+        self.musica = musica
+        self.proximo = None
